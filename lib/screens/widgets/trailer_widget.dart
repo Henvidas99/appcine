b8d468f310c55c6bb1ebab796560d1f184e7abb9
Henvidas99 <henryvartiel99@gmail.com>
2024-05-17 19:00:39 -0600

    detalle peliculas

--- lib/screens/widgets/trailer_widget.dart
+++ lib/screens/widgets/trailer_widget.dart
@@ -13,8 +13,8 @@ class TrailerWidget extends StatefulWidget {
 
 class _TrailerWidgetState extends State<TrailerWidget> {
   late YoutubePlayerController _controller;
-  late String _videoKey;
-  bool _isVideoLoaded = false;
+  late String _videoKey="";
+  bool _isControllerInitialized = false;
 
   @override
   void initState() {
@@ -23,85 +23,89 @@ class _TrailerWidgetState extends State<TrailerWidget> {
   }
 
   Future<void> _loadTrailerKey() async {
-    try {
-      final String? videoKey = await _getTrailerKey(widget.movie);
-      if (videoKey != null) {
-        setState(() {
-          _videoKey = videoKey;
-          _controller = YoutubePlayerController(
-            initialVideoId: _videoKey,
-            flags: YoutubePlayerFlags(
-              enableCaption: true,
-              autoPlay: false,
-              mute: false,
-              forceHD: true,
-            ),
-          );
-        });
-      } else {
-        // Handle error if trailer key is null
-        print('Error: Trailer key is null');
-      }
-    } catch (e) {
-      print('Error fetching trailer key: $e');
-      // Handle error
+  try {
+    final String? videoKey = await _getTrailerKey(widget.movie);
+    if (videoKey != null) {
+      setState(() {
+        _videoKey = videoKey;
+        _controller = YoutubePlayerController(
+          initialVideoId: _videoKey,
+          flags: YoutubePlayerFlags(
+            enableCaption: true,
+            autoPlay: false,
+            mute: false,
+            forceHD: true,
+          ),
+        );
+        _isControllerInitialized = true;
+      });
+    } else {
+      print('Error: Trailer key is null');
+      setState(() {
+
+        _controller = YoutubePlayerController(initialVideoId: ''
+        );
+        _isControllerInitialized = true;
+      });
     }
+  } catch (e) {
+    print('Error fetching trailer key: $e');
+    // Handle error
   }
+}
 
   Future<String?> _getTrailerKey(dynamic movie) async {
     final ApiService apiService = ApiService();
     return await apiService.fetchTrailerKey(movie['id']);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Show the movie poster image
-        Image.network(
+@override
+Widget build(BuildContext context) {
+  return _isControllerInitialized
+  ? _videoKey.isNotEmpty
+    ? YoutubePlayer(
+        thumbnail: Image.network(
           'https://image.tmdb.org/t/p/w500${widget.movie['poster_path']}',
           fit: BoxFit.cover,
-          width: double.infinity,
-          height: double.infinity,
         ),
-        // Show the YouTube player or a loading indicator
-        _isVideoLoaded
-            ? YoutubePlayer(
-                controller: _controller,
-                aspectRatio: 16 / 9,
-                showVideoProgressIndicator: true,
-                progressIndicatorColor: Colors.amber,
-                bottomActions: [
-                  CurrentPosition(),
-                  ProgressBar(
-                    isExpanded: true,
-                    colors: ProgressBarColors(
-                      playedColor: Colors.white,
-                      handleColor: Colors.amber,
-                    ),
-                  ),
-                  RemainingDuration(),
-                  FullScreenButton(),
-                ],
-              )
-            : Center(child: CircularProgressIndicator()),
-        // Show play button if video is loaded
-        if (_isVideoLoaded)
-          Positioned.fill(
-            child: Center(
-              child: IconButton(
-                icon: Icon(Icons.play_arrow),
-                iconSize: 50,
-                color: Colors.white,
-                onPressed: () {
-                  _controller.play();
-                },
-              ),
+        controlsTimeOut: const Duration(milliseconds: 1500),
+        aspectRatio: 16 / 9,
+        controller: _controller,
+        showVideoProgressIndicator: true,
+        bufferIndicator: Container(
+          color: Colors.black,
+          child: const Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
             ),
           ),
-      ],
+        ),
+        progressIndicatorColor: Colors.amber,
+        bottomActions: [
+          CurrentPosition(),
+          ProgressBar(
+            isExpanded: true,
+            colors: const ProgressBarColors(
+              playedColor: Colors.white,
+              handleColor: Colors.amber,
+            ),
+          ),
+          RemainingDuration(),
+          FullScreenButton(),
+        ],
+      )
+    : Container(
+        height: 200,
+        color: Colors.black, // Cambia el color del contenedor según sea necesario
+        child: const Center(child: Text("Trailer no disponible", style: TextStyle(fontSize: 16, color: Colors.white),))
+      )
+  : Container(
+      height: 200,
+      color: Colors.black, 
+      
     );
-  }
+}
+
 
   @override
   void dispose() {
